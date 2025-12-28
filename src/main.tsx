7c80653623bae1f6cec752b14eb3c8fad300f814
Elvin <elvin_hu@icloud.com>
2025-12-28 14:50:22 +0900

    Fix main.tsx - remove AuthGate import

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { AuthGate } from './components/AuthGate.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthGate>
-      <App />
-    </AuthGate>
+    <App />
   </StrictMode>,
 )
